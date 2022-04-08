83956035fe31ee780e3aad25b938837e5a167375
Blake Smethers <smethersblake@gmail.com>
2022-04-08 09:38:34 -0500

    add drag and drop feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -176,6 +176,62 @@ $('.list-group').on("blur", "input[type='text']", function() {
   // replace input with span element
   $(this).replaceWith(taskSpan)
 })
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: 'pointer',
+  helper: "clone",
+  active: function(event) {
+    console.log("deactive", this);
+  },
+  over: function(event) {
+    console.log('over', event.target)
+  },
+  out: function(event) {
+    console.log("out", event.target) 
+  },
+  update: function(event) {
+    var tempArr = []
+
+    $(this).children().each(function() {
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      var date = $(this)
+      .find('span')
+      .text()
+      .trim();
+      console.log(text, date)
+      tempArr.push({
+        text: text,
+        date: date
+      })
+    })
+    var arrName = $(this)
+    .attr('id')
+    .replace("list-", "");
+    tasks[arrName] = tempArr;
+    saveTasks()
+    
+  }
+})
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop")
+    ui.draggable.remove();
+  },
+  over:function(event, ui) {
+    console.log("over")
+  },
+  out: function(event, ui) {
+    console.log("out")
+  }
+})
+
 
 loadTasks();
 
