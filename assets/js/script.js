a8b794499df96111686df403852b43da80d1b25b
Blake Smethers <smethersblake@gmail.com>
2022-04-08 11:08:41 -0500

    add date feature and edit feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,7 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
+  auditTask(taskLi)
 
 
   // append to ul list on the page
@@ -142,12 +143,19 @@ $(".list-group").on("click", "span", function() {
 
   //swap out elements
   $(this).replaceWith(dateInput);
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      // when calendar is closed, force a "change" event on the 'dateInput
+      $(this).trigger("change")
+    }
+  })
 
   // automatically focu on new element
   dateInput.trigger("focus")
 })
 // value of due date was changed
-$('.list-group').on("blur", "input[type='text']", function() {
+$('.list-group').on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
   .val()
@@ -175,6 +183,9 @@ $('.list-group').on("blur", "input[type='text']", function() {
 
   // replace input with span element
   $(this).replaceWith(taskSpan)
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"))
 })
 $(".card .list-group").sortable({
   connectWith: $(".card .list-group"),
@@ -231,6 +242,31 @@ $("#trash").droppable({
     console.log("out")
   }
 })
+$('#modalDueDate').datepicker({
+  minDate: 1
+});
+var auditTask = function(taskEl) {
+  // get date from element
+  var date = $(taskEl).find("span").text().trim()
+  // to ensure it worked
+  console.log(date)
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17)
+  // remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger")
+  }
+  else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning")
+  }
+  //this should print out an object for the value of the date variable, but at 5:00pm of the date
+  console.log(time)
+}
+
 
 
 loadTasks();
